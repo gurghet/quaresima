3895a204375505ba3b4c81f4f0ea1f45d8026a44
Andrea Passaglia <apassaglia@gilt.com>
2017-10-26 17:02:11 +0100

    initial commit

---
+++ src/main/scala/com/gurghet/quaresima/HelloWorld.scala
@@ -0,0 +1,16 @@
+package com.gurghet.quaresima
+
+import io.circe._
+import org.http4s._
+import org.http4s.circe._
+import org.http4s.server._
+import org.http4s.dsl._
+import org.http4s.MediaType._
+
+object HelloWorld {
+  val service = HttpService {
+    case GET -> Root / "hello" / name =>
+      Ok("""<h2 style="font-size:200">Ciao Ania!</h2>""").withContentType(Some(`text/html`))
+      //Ok(Json.obj("message" -> Json.fromString(s"Hello, ${name}")))
+  }
+}
---
+++ src/main/scala/com/gurghet/quaresima/Server.scala
@@ -0,0 +1,15 @@
+package com.gurghet.quaresima
+
+import fs2.{Task, Stream}
+import org.http4s.util.StreamApp
+import org.http4s.server.Server
+import org.http4s.server.blaze.BlazeBuilder
+import scala.concurrent.ExecutionContext
+
+object BlazeExample extends StreamApp {
+  override def stream(args: List[String]): Stream[Task, Nothing] =
+    BlazeBuilder
+      .bindHttp(8080, "::")
+      .mountService(HelloWorld.service)
+      .serve
+}
