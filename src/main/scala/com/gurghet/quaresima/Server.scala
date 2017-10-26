289e983eea23f1ceddaf20faf0ae186f282b80c6
Andrea Passaglia <apassaglia@gilt.com>
2017-10-26 21:51:50 +0100

    0 => 0.0.0.0

--- src/main/scala/com/gurghet/quaresima/Server.scala
+++ src/main/scala/com/gurghet/quaresima/Server.scala
@@ -9,7 +9,7 @@ import scala.concurrent.ExecutionContext
 object BlazeExample extends StreamApp {
   override def stream(args: List[String]): Stream[Task, Nothing] =
     BlazeBuilder
-      .bindHttp(8080, "0")
+      .bindHttp(8080, "0.0.0.0")
       .mountService(HelloWorld.service)
       .serve
 }
